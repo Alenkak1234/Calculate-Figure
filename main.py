862d4a97280ee12f257be1f39272a5ce19361445
Alena <xmzmrp9wwbln@mail.ru>
2024-03-09 22:30:59 +0300

    added class

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
-def calculate_circle(radius = None):
-    if radius:
-        return radius ** 2 * 3.1415926
-
-def calculate_triangle(a = None, b = None, c = None):
-    if a and b and c:
-        p = 1/2 * (a + b + c)
-        return (p * (p - a) * (p - b) * (p - c)) ** (1/2)
+from abc import ABC, abstractmethod
 
 
-def calculate_square(type=None, **kwargs):
-    '''Calculates square of a figure
-    Parameters:
-        type of a figure (str)
-    Returns:
-        square(int)
-    '''
-    if type == 'circle':
-        return calculate_circle(**kwargs)
-    if type == 'triangle':
-        return calculate_triangle(**kwargs)
+class Figures(ABC):
+    @abstractmethod
+    def calculate_square(self):
+        pass
 
 
+class Figure(Figures):
+    """Class square of a figure
+        Parameters:
+            type of figure (str)
+            necessary data to calculate
+        Returns:
+            square(int)
+        """
 
+    def __init__(self, type):
+        self.type = type
+
+    def calculate_square(self, **kwargs):
+        if self.type == 'circle':
+            return self.calculate_circle(**kwargs)
+        elif self.type == 'triangle':
+            return self.calculate_triangle(**kwargs)
+
+    def calculate_circle(self, radius=None):
+        if radius:
+            return radius ** 2 * 3.1415926
+
+    def calculate_triangle(self, a=None, b=None, c=None):
+        if a and b and c:
+            p = 1 / 2 * (a + b + c)
+            return (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
+
+
+figure = Figure('circle')
+square = figure.calculate_square(radius=2)
+print(square)
+figure = Figure('triangle')
+square = figure.calculate_square(a=4, b=4, c=2)
+print(square)
